02aef1bafea3a45409b84d666545da2e6cff2f08
AmooHashem <amoo.hashe.mehraban@gmail.com>
2020-02-18 03:48:56 +0330

    start to add dbs!

--- main/main.py
+++ main/main.py
@@ -1,31 +1,66 @@
-from telegram import KeyboardButton, ReplyKeyboardMarkup, ParseMode, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, InlineQueryHandler, CommandHandler, CallbackQueryHandler
-import requests
-import re
+import sqlite3
+
+from telegram import KeyboardButton, ReplyKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 my_token = '996901425:AAHeL5pN03vZZ2KouvIMbuhyJq4YrpRH3tI'
-
-
-# def add_team(bot, update, cursor, num, name):
-#     cursor.execute('insert into teams values (?,?,?)', num, name, 0)
+conn = sqlite3.connect('bot.db')
+c = conn.cursor()
+c.execute('create table users (chat_id int,state string);')
+c.execute('create table teams (number int,name string,score real);')
 
 
 def start(update, context):
-    keyboard = [[KeyboardButton("افزودن تیم", callback_data='add_team'),
-                 KeyboardButton("حذف تیم", callback_data='delete_team')],
-                [KeyboardButton("افزودن سوال", callback_data='add_question'),
-                 KeyboardButton("حذف سوال", callback_data='delete_question')]]
+    bot = update.message.bot
+    message = update.message
+
+    print(message.chat_id)
+    print(type(message.chat_id))
+    # add user to database
+    c.execute('insert into users values ({},{});'.format(message.chat_id, "\'menu\'"))
+
+    print(":))")
+
+    # show initial messages to user
+    keyboard = [[KeyboardButton("Change score"), KeyboardButton("Assign problem")],
+                [KeyboardButton("Suspend a team"), KeyboardButton("Show team solved problems")],
+                [KeyboardButton("Show team in-hand problems")]]
+    reply_markup = ReplyKeyboardMarkup(keyboard)
+    bot.send_message(
+        'سلام منتور عزیز!\nخسته نباشی!\nایشالا مسابقه ی خوبی در پیش داشته باشیم!\n', reply_markup=reply_markup)
+
+
+def admin(update, context):
+    bot = update.message.bot
+    message = update.message
+
+    # change state of user to "admin" mode
+    c.execute("update users set state = {} where chat_id = {}".format("\'admin\'", message.chat_id))
+
+    keyboard = [[KeyboardButton("Add team"), KeyboardButton("Delete team")],
+                [KeyboardButton("Add question"), KeyboardButton("Delete question")]]
 
     reply_markup = ReplyKeyboardMarkup(keyboard)
+    bot.send_message(chat_id=message.chat_id, text='سلام ادمین!\nیکی از گزینه های زیر رو انتخاب کن:',
+                     reply_markup=reply_markup)
+
+
+def messages(update, context):
     bot = update.message.bot
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text='یکی از گزینه های زیر رو انتخاب کنید:', reply_markup=reply_markup)
+    message = update.message
 
+    state = c.execute("select state from users where chat_id = {}".format(message.chat_id)).fetchall()[0][0]
 
-def admin_buttons(update, context):
-    query = update.callback_query
+    print(state + "???????????????????????")
 
-    query.edit_message_text(text='لطفا شماره ی تیم را وارد کنید.')
+    if state == "افزودن تیم":
+        bot.send_message(":)")
+    if state == "delete_team":
+        pass
+    if state == "add_question":
+        pass
+    if state == "delete_question":
+        pass
 
 
 def main():
@@ -33,7 +68,8 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CallbackQueryHandler(admin_buttons))
+    dp.add_handler(MessageHandler(Filters.text, messages))
+
     # dp.add_handler(CommandHandler('Add team', add_team))
     # dp.add_handler(CommandHandler('Delete team', del_team))
 
